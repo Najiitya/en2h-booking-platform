f4729e2dcdc64b662cd30a78ba5555c4a21b4507
Najith Nethwan <nethwannajith@gmail.com>
2026-07-11 12:33:14 +0530

    feat: add Swagger integration for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Booking Platform API')
+    .setDescription('API documentation for the Booking Platform')
+    .setVersion('1.0')
+    .addBearerAuth() // Add this line to include Bearer Auth in Swagger
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
